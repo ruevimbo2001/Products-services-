5f099ab1974bba05a1780aebcb3a49a6bba7f6c1
Gracious Sakarombe <gracious.sakarombe@telco.co.zw>
2024-08-23 17:05:29 +0200

    update2

--- HairBraiding/admin.py
+++ HairBraiding/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import HairbraidingService
 
-admin.site.register('HairbraidingService')
+# admin.site.register('HairbraidingService')
 @admin.register(HairbraidingService)
 class HairbraidingServiceAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'duration')
